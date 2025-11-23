e4cd7c81b538577f8d52863fd145cac72edf327a
sergiohdljr <sergio.tjf1@gmail.com>
2025-11-23 12:02:43 -0300

    chore: update Drizzle configuration to output migrations to src/db/migrations directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 export default defineConfig({
   schema: './src/db/schema.ts',
-  out: './drizzle',
+  out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,

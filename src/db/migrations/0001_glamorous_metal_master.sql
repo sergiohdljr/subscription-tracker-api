9c5ebf30a8f509ef40ef5419600bb462b57e49fe
sergiohdljr <sergio.tjf1@gmail.com>
2025-11-23 12:55:04 -0300

    feat: add subscription schema and repository

--- src/db/migrations/0001_glamorous_metal_master.sql
+++
@@ -1,4 +0,0 @@
-CREATE TYPE "public"."billing_cycle" AS ENUM('weekly', 'monthly', 'yearly');--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "billing_cycle" SET DEFAULT 'monthly'::"public"."billing_cycle";--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "billing_cycle" SET DATA TYPE "public"."billing_cycle" USING "billing_cycle"::"public"."billing_cycle";--> statement-breakpoint
-ALTER TABLE "subscriptions" ADD COLUMN "active" boolean DEFAULT true NOT NULL;

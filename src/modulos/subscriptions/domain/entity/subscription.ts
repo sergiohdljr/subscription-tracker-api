896d15bc57f5840f26f0786ebb867a4d37e56a28
sergiohdljr <sergio.tjf1@gmail.com>
2026-01-11 12:53:29 -0300

    feat: add Subscription and Money value objects for subscription management
    - Introduce `Subscription` class to encapsulate subscription details, including status, billing cycle, and renewal logic.
    - Implement `Money` class to handle monetary values with currency support and arithmetic operations.
    - Enhance subscription management capabilities with methods for status checks and renewal notifications.

---
+++ src/modulos/subscriptions/domain/entity/subscription.ts
@@ -0,0 +1,57 @@
+import { BillingCycle } from "../value-objects/billing-cycle";
+import { Money } from "../value-objects/money";
+
+export type SubscriptionStatus = "ACTIVE" | "CANCELED" | "TRIAL";
+export type Currency = "BRL" | "USD";
+
+export class Subscription {
+    constructor(
+        public readonly id: string,
+        public readonly userId: string,
+        public name: string,
+        public price: Money,
+        public currency: Currency,
+        public billingCycle: BillingCycle,
+        public status: SubscriptionStatus,
+        public startDate: Date,
+        public nextBillingDate: Date,
+        public lastBillingDate: Date | null,
+        public renewalNotifiedAt: Date | null,
+        public trialEndsAt: Date | null,
+        public readonly createdAt: Date,
+        public updatedAt: Date
+    ) { }
+
+    public isActive() {
+        return this.status === "ACTIVE"
+    }
+
+    public isTrial() {
+        return this.status === "TRIAL"
+    }
+
+    public isCanceled() {
+        return this.status == "CANCELED"
+    }
+
+    public renew() {
+        if (!this.isActive()) return;
+
+        this.lastBillingDate = this.nextBillingDate;
+        this.nextBillingDate = this.billingCycle.nextDate(this.nextBillingDate);
+        this.renewalNotifiedAt = null;
+        this.updatedAt = new Date();
+    }
+
+
+    public shouldNotify(daysBefore: number, today: Date) {
+        if (this.isCanceled()) return false;
+        if (this.renewalNotifiedAt) return false;
+
+        const diff =
+            (this.nextBillingDate.getTime() - today.getTime()) /
+            (1000 * 60 * 60 * 24);
+
+        return diff <= daysBefore && diff >= 0;
+    }
+}

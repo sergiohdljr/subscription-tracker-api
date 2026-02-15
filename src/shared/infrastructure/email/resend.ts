843b00960a0327ec4fa2b6dacfce2f72664d3df3
sergiohdljr <sergio.tjf1@gmail.com>
2026-02-15 13:03:28 -0300

    refactor: update Resend configuration to use adapter pattern
    - Renamed ResendConfig to ResendConfigAdapter for clarity.
    - Added methods to fetch email templates and send emails using the Resend API.
    - Improved error handling for missing templates.
    - Updated instance export to reflect the new adapter structure.

--- src/shared/infrastructure/email/resend.ts
+++ src/shared/infrastructure/email/resend.ts
@@ -1,13 +1,32 @@
 import { Resend } from 'resend';
 import * as dotenv from 'dotenv';
 
-export class ResendConfig {
-  constructor(private readonly API_KEY: string) {}
+export class ResendConfigAdapter {
+  constructor(private readonly API_KEY: string) { }
 
   getInstance() {
     return new Resend(this.API_KEY);
   }
+
+  async getTemplate(templateId: string) {
+    const template = await this.getInstance().templates.get(templateId);
+    return template;
+  }
+
+  async sendEmail(email: string, subject: string, templateId: string, data: Record<string, string | number>) {
+    const template = await this.getTemplate(templateId);
+    if (!template.data) {
+      throw new Error('Template not found');
+    }
+
+    return this.getInstance().emails.send({
+      subject,
+      from: process.env.RESEND_FROM_EMAIL ?? 'no-reply@subscription-tracker.com',
+      to: email,
+      template: { id: template.data.id, variables: data },
+    });
+  }
 }
 
 dotenv.config();
-export const resendConfig = new ResendConfig(process.env.RESEND_API_KEY ?? '');
+export const resendAdapter = new ResendConfigAdapter(process.env.RESEND_API_KEY ?? '');

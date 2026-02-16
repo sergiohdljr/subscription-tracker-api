c564e337af588ca2fc825c0a068994eb3c746bb8
sergiohdljr <sergio.tjf1@gmail.com>
2026-02-16 16:24:27 -0300

    feat: add saveMany method to SubscriptionRepository interface
    - Introduced saveMany method to allow batch saving of subscriptions, returning an array of results.

--- src/modules/subscriptions/application/repositories/subscriptions-repository.ts
+++ src/modules/subscriptions/application/repositories/subscriptions-repository.ts
@@ -2,6 +2,7 @@ import type { Subscription } from '../../domain/entity/subscription';
 
 export interface SubscriptionRepository {
   save(subscription: Subscription): Promise<Record<string, number>>;
+  saveMany(subscriptions: Subscription[]): Promise<Array<Record<string, number>>>;
   updateMany(subscriptions: Subscription[]): Promise<void>;
   findById(id: number, userId: string): Promise<Subscription | null>;
   findByUserId(userId: string): Promise<Subscription[]>;
